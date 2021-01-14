e58ff9e9f8e4b82201d1da1d28cde0b51f05a542
Suha Siddiqui <suhasid@my.yorku.ca>
2021-01-14 14:52:31 -0500

    hi

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("New Version 17: Martin Edition 3");
+		System.out.println("New Version 17: Martin Edition 2");
 		System.out.println("New Version Rayta");
-		System.out.println("New Version Suha Siddiqui 2");
+		System.out.println("New Version Suha Siddiqui");
 		System.out.println("Richard version");
 	}
 
