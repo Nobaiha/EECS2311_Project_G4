25aa557eaefd6732138a4ae0880f5708b7524261
dokdek <dokdek@gmail.com>
2021-01-14 21:57:41 +0000

    Patchanon edit

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		System.out.println("Richard version");
 		System.out.println("Rayta version 2");
 		System.out.println("Suha version 2");
+		System.out.println("Patchanon edit");
 	}
 
 }
