9ce165eb96d638610faac76f0da310428c1f096d
Martin-Brejniak <martin.brejniak@bell.net>
2021-01-14 14:48:47 -0500

    Martin did this 5.

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("New Version 17: Martin Edition 2");
+		System.out.println("New Version 17: Martin Edition 3");
 		System.out.println("New Version Rayta");
 		System.out.println("New Version Suha Siddiqui");
 		System.out.println("Richard version");
