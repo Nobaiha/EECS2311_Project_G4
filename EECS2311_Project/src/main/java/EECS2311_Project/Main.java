41ef5225623f607607ae5e51e33cfb619a27e5ae
Zhilong Lin <lzl1114@my.yorku.ca>
2021-01-25 12:39:39 -0500

    conflict trying

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -17,6 +17,7 @@ public class Main {
 		System.out.println("Patchanon edit");*/
 		//Rayta labtask
 		//Suha labtask
+		//conflict trying(Zhilong Lin)
 
 		ArrayList<String> testLines = new ArrayList<>();
 
