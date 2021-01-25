fda4559f3c2485cc70ad0ec93fe43de8eed08a31
Martin-Brejniak <martin.brejniak@bell.net>
2021-01-25 12:40:31 -0500

    New commit

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*System.out.println("New Version 17: Martin Edition 2");
+		 * New change
 		System.out.println("New Version Rayta");
 		System.out.println("New Version Suha Siddiqui");
 		System.out.println("Richard version");
