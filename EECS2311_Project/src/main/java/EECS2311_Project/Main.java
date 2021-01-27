2b4e646e88dbc68ab8d09c0171b39b99fa7c6668
Martin-Brejniak <martin.brejniak@bell.net>
2021-01-27 13:40:59 -0500

    Conflict fix Martin

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -109,11 +109,10 @@ public class Main {
 
 
 		//conflict here? conflicts 
-<<<<<<< HEAD
+
 		//pushing try ZL
-=======
         //change suha
->>>>>>> branch 'master' of https://github.com/Nobaiha/EECS2311_Project_G4.git
+
 
 
 
