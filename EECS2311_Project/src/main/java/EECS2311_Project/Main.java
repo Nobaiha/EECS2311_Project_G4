73f499641c7183b50dc5b19e7d39fff5ad63ef20
Nobaiha Zaman Rayta <nobaiha@my.yorku.ca>
2021-01-27 01:30:51 -0500

    Rayta tring lab2

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -17,7 +17,6 @@ public class Main {
 		System.out.println("Patchanon edit");*/
 		//Rayta labtask
 
-		//Rayta_trying_labtask2
 		ArrayList<String> testLines = new ArrayList<>();
 
 		//might need to change the file path depending on system.
