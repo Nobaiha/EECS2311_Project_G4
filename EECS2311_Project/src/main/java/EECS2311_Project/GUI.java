5ec923d30018bf07515669145e5fdabafe032362
Suha Siddiqui <suhasid@my.yorku.ca>
2021-01-30 18:03:02 -0500

    GUI framework

---
+++ EECS2311_Project/src/main/java/EECS2311_Project/GUI.java
@@ -0,0 +1,39 @@
+package EECS2311_Project;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI {
+	public GUI() {
+		JFrame frame = new JFrame();
+		
+		JButton button = new JButton("Click me");
+		JLabel label = new JLabel("Number of clicks: 0");
+		
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		panel.add(label);
+		
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Our GUI");
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GUI();
+	}
+
+}
