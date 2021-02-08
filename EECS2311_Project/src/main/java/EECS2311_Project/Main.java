52593a237123159669426a61f68d177764d2f8d1
Martin-Brejniak <martin.brejniak@bell.net>
2021-02-07 23:03:08 -0500

    drumXMLParser method started

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -30,22 +30,22 @@ public class Main {
         ArrayList<String> testLines = new ArrayList<>();
         ArrayList<String> drumTestLines = new ArrayList<>();
 
-        //might need to change the file path depending on system.
+//might need to change the file path depending on system.
         File inputFile = new File(file);
 
-        //need to check for some others too, sometimes E is a D?
-        //string tuning can be changed, needs to be accounted for
+//need to check for some others too, sometimes E is a D?
+//string tuning can be changed, needs to be accounted for
         Pattern pattern = Pattern.compile("^([eABCDEFG])");
-        //Need to check for alternatives such as CC HH, etc.
+//Need to check for alternatives such as CC HH, etc.
         Pattern drumPattern = Pattern.compile("^([CHSBRTF])");
 
-        //Reads the txt file and parses each line into the arraylist if it is a note line.
-        //Maybe need to edit later as sometimes there is information on top of the bars???
+//Reads the txt file and parses each line into the arraylist if it is a note line.
+//Maybe need to edit later as sometimes there is information on top of the bars???
         try (Scanner sc = new Scanner(inputFile)) {
             while (sc.hasNextLine()) {
-                //removes all spaces.
+//removes all spaces.
                 String nextLine = sc.nextLine().replaceAll("\\s", "");
-                //System.out.println(nextLine);
+//System.out.println(nextLine);
                 Matcher matcher = pattern.matcher(nextLine);
                 if (matcher.find()) {
                     testLines.add(nextLine);
@@ -57,7 +57,7 @@ public class Main {
                 }
             }
         } catch (FileNotFoundException e) {
-            //Send error not found here.
+//Send error not found here.
             e.printStackTrace();
         }
         if (testLines.size() > drumTestLines.size()) {
@@ -73,11 +73,11 @@ public class Main {
    	 * @return an Array of a notes Part, Measure number, Element number, and Element value.
    	 */
     public static ArrayList<GuitarNote> guitarNoteParser (ArrayList<String> noteArray){
-        //Creates array of notes
+//Creates array of notes
         ArrayList<String> strArray = new ArrayList<>();
         for (String list : noteArray) {
             String[] tempArray = list.split("-", -1);
-            //converts to arraylist, prob a better way to do this.
+//converts to arraylist, prob a better way to do this.
             Collections.addAll(strArray, tempArray);
         }
 
@@ -90,34 +90,34 @@ public class Main {
         int measureMem = 1;
         Pattern pattern = Pattern.compile("^([eABCDEFG])");
 
-        //Iterates through the parsed string array and makes an array of note objects
+//Iterates through the parsed string array and makes an array of note objects
         for (String str : strArray) {
-            //System.out.println(str);
-            //checks for old regex expression which represents a note (e,a,b,d...)
+//System.out.println(str);
+//checks for old regex expression which represents a note (e,a,b,d...)
             Matcher matcher = pattern.matcher(str);
             if (matcher.find()) {
-                //If it has gone through 6 iterations (6 strings) dont reset the measure count
+//If it has gone through 6 iterations (6 strings) dont reset the measure count
                 if (repeatTimes == 5) {
                     repeatTimes = 0;
                     measureMem = measureNum;
                 } else {
                     measureNum = measureMem;
                 }
-                //Sets string # to the index at which the char is located, 0 represents e 1 represents A... etc.
+//Sets string # to the index at which the char is located, 0 represents e 1 represents A... etc.
                 noteNum = 1;
                 stringVal = str.trim().toLowerCase().charAt(0);
                 repeatTimes++;
             } else if (str.equals("|")) {
-                //Increase measure count if it encounters |
+//Increase measure count if it encounters |
                 noteNum = 1;
                 measureNum++;
             } else if (str.length() == 0) {
-                //if blank, increase note count.
+//if blank, increase note count.
                 noteNum++;
             } else {
-                //Otherwise create a new note with the string as the note value.
-                //splits each "block" into smaller individual notes, only checks for alphabet chars in between right now
-                //will update regex when encountering new patterns.
+//Otherwise create a new note with the string as the note value.
+//splits each "block" into smaller individual notes, only checks for alphabet chars in between right now
+//will update regex when encountering new patterns.
                 for (String character : str.split("(?<=[a-z])")) {
                     noteNum++;
                     GuitarNote tempGuitarNote = new GuitarNote(measureNum, noteNum, stringVal, character);
@@ -142,12 +142,12 @@ public class Main {
    	 * @return an Array of a notes Part, Measure number, Element number, and Element value.
    	 */
     public static ArrayList<DrumNote> drumNoteParser (ArrayList<String> noteArray){
-        //drum test case
+//drum test case
         ArrayList<DrumNote> drumNoteArray = new ArrayList<>();
         ArrayList<String> strArray = new ArrayList<>();
         for (String list : noteArray) {
             String[] tempArray = list.split("-", -1);
-            //converts to arraylist, prob a better way to do this.
+//converts to arraylist, prob a better way to do this.
             Collections.addAll(strArray, tempArray);
         }
 
@@ -158,27 +158,27 @@ public class Main {
         int measureMem = 1;
         Pattern pattern = Pattern.compile("^([CHSBRTF])");
 
-        //Iterates through the parsed string array and makes an array of note objects
+//Iterates through the parsed string array and makes an array of note objects
         for (String str : strArray) {
-            //System.out.println((str));
-            //checks for old regex expression which represents a note
+//System.out.println((str));
+//checks for old regex expression which represents a note
             Matcher matcher = pattern.matcher(str);
             if (matcher.find()) {
-                //System.out.println((str));
-                //checks to see if the first part of the line indicates a "string" i.e checks for a |
+//System.out.println((str));
+//checks to see if the first part of the line indicates a "string" i.e checks for a |
                 int indexOfMeasure = str.indexOf("|");
                 if (indexOfMeasure != -1) {
                     part = str.substring(0, indexOfMeasure);
-                    //does once, to set the repeated char to the first string.
+//does once, to set the repeated char to the first string.
                     if (repeatedPart.equals("")) {
                         repeatedPart = part;
                     }
                     if (str.length() > indexOfMeasure + 1) {
-                        //outlier where first note isnt a '-'
+//outlier where first note isnt a '-'
                         DrumNote tempDrumNote = new DrumNote(measureNum, noteNum, part, str.charAt(indexOfMeasure + 1));
                         drumNoteArray.add(tempDrumNote);
                     } else if (repeatedPart.equals(part)) {
-                        //If it has gone through all possible parts dont reset the measure count
+ //If it has gone through all possible parts dont reset the measure count
                         measureMem = measureNum;
                     } else {
                         measureNum = measureMem;
@@ -186,23 +186,23 @@ public class Main {
                     noteNum = 1;
                 }
             } else if (str.contains("|")) {
-                //Increase measure count if it encounters |
+//Increase measure count if it encounters |
                 noteNum = 1;
                 measureNum++;
                 if (str.length() > 1) {
-                    //Extract this to separate function? duplicate code in function below.
+//Extract this to separate function? duplicate code in function below.
                     for (char character : str.substring(1).toCharArray()) {
                         DrumNote tempDrumNote = new DrumNote(measureNum, noteNum, part, character);
                         drumNoteArray.add(tempDrumNote);
                     }
                 }
             } else if (str.length() == 0) {
-                //if blank, increase note count.
+//if blank, increase note count.
                 noteNum++;
             } else {
-                //Otherwise create a new note with the string as the note value.
-                //splits each "block" into smaller individual notes, only checks for alphabet chars in between right now
-                //will update regex when encountering new patterns.
+//Otherwise create a new note with the string as the note value.
+//splits each "block" into smaller individual notes, only checks for alphabet chars in between right now
+//will update regex when encountering new patterns.
                 for (char character : str.toCharArray()) {
                     noteNum++;
                     DrumNote tempDrumNote = new DrumNote(measureNum, noteNum, part, character);
@@ -228,7 +228,7 @@ public class Main {
    	 * @return an XML file.
    	 */
     public static void guitarXMLParser (ArrayList<GuitarNote> guitarNoteArray){
-        //XML print attempt
+//XML print attempt
         Directives directives = new Directives();
         directives
                 .add("score-partwise")
@@ -269,7 +269,7 @@ public class Main {
                     .up();
             for (GuitarNote guitarNote : guitarNoteArray) {
                 if (guitarNote.measure == i + 1) {
-                    //process note here later
+//process note here later
                     directives
                             .add("note")
                             .add("pitch")
@@ -315,7 +315,7 @@ public class Main {
    	 * @return an XML file.
    	 */
     public static void drumXMLParser (ArrayList<DrumNote> drumNoteArray){
-        //XML print attempt
+//XML print attempt
         Directives directives = new Directives();
         directives
                 .add("score-partwise")
@@ -354,14 +354,14 @@ public class Main {
                     .up()
                     .up()
                     .up();
-            for (GuitarNote guitarNote : guitarNoteArray) {
-                if (guitarNote.measure == i + 1) {
-                    //process note here later
+            for (DrumNote drumNote : drumNoteArray) {
+                if (drumNote.measure == i + 1) {
+//process note here later
                     directives
                             .add("note")
                             .add("pitch")
                             .add("step")
-                            .set(guitarNote.noteValue)
+                            .set(drumNote.noteValue)
                             .up()
                             .add("octave")
                             .set(4)
