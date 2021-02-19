ce19446d6d1edc9f8657850a53a659f7d36c2afe
Martin-Brejniak <martin.brejniak@bell.net>
2021-02-19 17:45:07 -0500

    Java Doc Updated V2

--- EECS2311_Project/src/main/java/EECS2311_Project/Main.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Main.java
@@ -18,11 +18,26 @@ public class Main {
     static HashMap<Integer, Integer> measuresElement = new HashMap<>();
     static ArrayList<Measure> measures = new ArrayList<>();
 
+    /**
+	 * This checks to see if the input file is a ".txt" file.
+	 * 
+	 * @param file is the name of the input file.
+	 * @return true if the file is of type ".txt". Fails if otherwise.
+	 */
     public static boolean fileChecker(String file) {
         int indexOfExt = file.lastIndexOf(".") + 1;
         return file.substring(indexOfExt).equals("txt");
     }
 
+    /**
+	 * This reads the text file, extracts the infomation, and 
+	 * places it in an ArrayList. It detects whether its a
+	 * drum or guitar tab.  
+	 * 
+	 * @param file is the name of the input file.
+	 * @return an ArrayList of the guitar/drum notes.
+	 * @exception It throws FileNotFoundException if the file is not found.
+	 */
     public static Object[] fileParser(String file) throws FileNotFoundException {
         if (!fileChecker(file)) {
             throw new FileNotFoundException("Unsupported file type");
@@ -67,6 +82,13 @@ public class Main {
         }
     }
 
+    /**
+	 * This takes the information for the guitar tab ArrayList, picks out
+	 * the important information, and places them into variables.
+	 * 
+	 * @param noteArray is the ArrayList with the converted ".txt" file.
+	 * @return an ArrayList of the guitar tab information.
+	 */
     public static ArrayList<GuitarNote> guitarNoteParser(ArrayList<String> noteArray) {
     	//Creates array of notes
         ArrayList<String> strArray = new ArrayList<>();
@@ -144,6 +166,13 @@ public class Main {
         return guitarNoteArray;
     }
 
+    /**
+	 * This takes the information for the drum tab ArrayList, picks out
+	 * the important information, and places them into variables.
+	 * 
+	 * @param noteArray is the ArrayList with the converted ".txt" file.
+	 * @return an ArrayList of the drum tab information.
+	 */
     public static ArrayList<DrumNote> drumNoteParser(ArrayList<String> noteArray) {
         //drum test case
         ArrayList<DrumNote> drumNoteArray = new ArrayList<>();
@@ -225,6 +254,13 @@ public class Main {
         return drumNoteArray;
     }
 
+    /**
+	 * Takes all the information gathered from the guitar tab
+	 * and makes an XML file with it.
+	 * 
+	 * @param measyres is an ArrayList of the guitar tab information.
+	 * @return an XML file.
+	 */
     public static String guitarXMLParser(ArrayList<Measure> measures) {
         //XML print attempt
         if (measures.size() == 0) {
@@ -382,6 +418,13 @@ public class Main {
         //System.out.println(xml);
     }
 
+    /**
+	 * Takes all the information gathered from the drum tab
+	 * and makes an XML file with it.
+	 * 
+	 * @param drumNoteArray is an ArrayList of the drum tab information.
+	 * @return an XML file.
+	 */
     public static String drumXMLParser(ArrayList<DrumNote> drumNoteArray) {
         if (drumNoteArray.size() == 0) {
             return null;
@@ -461,6 +504,12 @@ public class Main {
         //System.out.println(xml);
     }
 
+    /**
+	 * This saves the XML file that was created.
+	 * 
+	 * @param file is the file that was input.
+	 * @param xml is the XML file.
+	 */
     public static void saveFile(File file, String xml) {
         try {
             FileWriter xmlFile = new FileWriter(file);
@@ -482,6 +531,9 @@ public class Main {
         return null;
     }*/
 
+    /**
+	 * TODO
+	 */
     public static void start(String filePath) throws FileNotFoundException {
         Object[] notes = fileParser(filePath);
         ArrayList<String> noteArray = (ArrayList<String>) notes[1];
@@ -528,6 +580,9 @@ public class Main {
         }
     }
 
+    /**
+	 * This starts up the GUI.
+	 */
     public static void main(String[] args) {
         GuiWelcome welcomePage = new GuiWelcome();
         welcomePage.setVisible(true);
