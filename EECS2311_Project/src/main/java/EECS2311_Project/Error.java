dad4ab33194146af6d8b2a2e62b1357c4a5bbf02
dokdek <dokdek@gmail.com>
2021-02-27 15:45:24 -0500

    Fixed issue with creating extra jframes & added custom error msg

--- EECS2311_Project/src/main/java/EECS2311_Project/Error.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/Error.java
@@ -2,10 +2,11 @@ package EECS2311_Project;
 
 import javax.swing.*;
 
-public class Error extends JFrame {
-    public Error() {
-        int input = JOptionPane.showOptionDialog(null, "We had an issue processing your tab, please retry.", "Error", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+public class Error {
+    public Error(String error) {
+        int input = JOptionPane.showOptionDialog(null, error, "Error", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
         if (input == JOptionPane.OK_OPTION) {
+            JOptionPane.getRootFrame().dispose();
             new GuiWelcome().setVisible(true);
         } else {
             System.exit(0);
