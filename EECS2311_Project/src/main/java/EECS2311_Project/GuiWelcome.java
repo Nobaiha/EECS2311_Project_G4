abb012f071c14a2910e5b68ab9ce659f695fc03c
Suha Siddiqui <suhasid@my.yorku.ca>
2021-03-30 02:27:50 -0400

    welcome page modified

--- EECS2311_Project/src/main/java/EECS2311_Project/GuiWelcome.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/GuiWelcome.java
@@ -17,6 +17,10 @@ import java.awt.event.MouseEvent;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.border.MatteBorder;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
 
 public class GuiWelcome extends JFrame {
 
@@ -46,23 +50,13 @@ public class GuiWelcome extends JFrame {
 		setBounds(100, 100, 919, 805);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(248, 248, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		Label tmLabel = new Label("TM");
-		tmLabel.setBounds(434, 10, 28, 21);
-		contentPane.add(tmLabel);
-		
-		Label standardHeaderLabel = new Label("TAB-2-MusicXML");
-		standardHeaderLabel.setForeground(new Color(0, 51, 153));
-		standardHeaderLabel.setFont(new Font("Arial Black", Font.BOLD, 40));
-		standardHeaderLabel.setBounds(10, 25, 418, 49);
-		contentPane.add(standardHeaderLabel);
 		
 		//ModificationsPage modificationsPage = new ModificationsPage();
 		GuiUploadWindow guiUploadWindow = new GuiUploadWindow("","","");
 		Button browseButton = new Button("Start");
+		browseButton.setBackground(new Color(255, 255, 255));
 		browseButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -79,25 +73,67 @@ public class GuiWelcome extends JFrame {
 		instructionLabel2.setBounds(131, 473, 646, 49);
 		contentPane.add(instructionLabel2);
 		
-		Label instructionLabel1 = new Label("Welcome to your personal tablature to music XML converter!");
-		instructionLabel1.setAlignment(Label.CENTER);
-		instructionLabel1.setFont(new Font("Calibri", Font.PLAIN, 28));
-		instructionLabel1.setBounds(48, 169, 823, 49);
-		contentPane.add(instructionLabel1);
-		
-		JLabel lblNewLabel = new JLabel("The TAB-2-MusicXML\u2122 (T.2.M) converter takes TABs of guitar music,\r\n");
-		lblNewLabel.setForeground(Color.BLUE);
+		JLabel lblNewLabel = new JLabel("Welcome to TAB-2-MusicXML\u2122 (T.2.M) converter that takes TABs of guitar \r\n");
+		lblNewLabel.setForeground(new Color(102, 102, 255));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Calibri", Font.BOLD | Font.ITALIC, 24));
 		lblNewLabel.setBounds(33, 300, 838, 49);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("and converts them into a musicXML. Enjoy!\r\n");
-		lblNewLabel_1.setForeground(Color.BLUE);
+		JLabel lblNewLabel_1 = new JLabel("and bass music converts them into a musicXML. Enjoy!\r\n");
+		lblNewLabel_1.setForeground(new Color(102, 102, 255));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Calibri", Font.BOLD | Font.ITALIC, 24));
 		lblNewLabel_1.setBounds(33, 359, 838, 49);
 		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("  TAB - 2 - MusicXML\u2122");
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setFont(new Font("Poor Richard", Font.BOLD, 50));
+		lblNewLabel_2.setBackground(new Color(153, 153, 204));
+		lblNewLabel_2.setBounds(0, 0, 905, 67);
+		lblNewLabel_2.setOpaque(true);
+		contentPane.add(lblNewLabel_2);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(52, 438, 801, 11);
+		contentPane.add(separator);
+		
+		JButton btnNewButton = new JButton("Learn More");
+		btnNewButton.setFont(new Font("Calibri", Font.PLAIN, 15));
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setBounds(309, 728, 109, 30);
+		contentPane.add(btnNewButton);
+		
+		JButton btnTutorial = new JButton("Tutorial");
+		btnTutorial.setFont(new Font("Calibri", Font.PLAIN, 15));
+		btnTutorial.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnTutorial.setBackground(new Color(255, 255, 255));
+		btnTutorial.setBounds(562, 728, 109, 30);
+		contentPane.add(btnTutorial);
+		
+		JLabel lblNewLabel_3 = new JLabel("User manual:\r\n");
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setFont(new Font("Calibri", Font.BOLD | Font.ITALIC, 18));
+		lblNewLabel_3.setBounds(178, 727, 121, 30);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Live demo:\r\n");
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setFont(new Font("Calibri", Font.BOLD | Font.ITALIC, 18));
+		lblNewLabel_4.setBounds(470, 731, 93, 23);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("");
+		lblNewLabel_2_2.setOpaque(true);
+		lblNewLabel_2_2.setForeground(Color.WHITE);
+		lblNewLabel_2_2.setFont(new Font("Poor Richard", Font.BOLD, 50));
+		lblNewLabel_2_2.setBackground(new Color(153, 153, 204));
+		lblNewLabel_2_2.setBounds(0, 701, 905, 67);
+		contentPane.add(lblNewLabel_2_2);
 		
 		/*JLabel logoImageLabel = new JLabel("");
 		logoImageLabel.setIcon(new ImageIcon(GuiWelcome.class.getResource("/EECS2311_Project/imgs/TAB-2-MusicXML.png")));
