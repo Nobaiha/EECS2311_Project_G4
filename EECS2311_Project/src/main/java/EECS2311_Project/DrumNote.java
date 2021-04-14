fc501120e9615873023fec86d5eaf09d77030529
Martin-Brejniak <martin.brejniak@bell.net>
2021-04-13 20:05:38 -0400

    Small update.

--- EECS2311_Project/src/main/java/EECS2311_Project/DrumNote.java
+++ EECS2311_Project/src/main/java/EECS2311_Project/DrumNote.java
@@ -6,10 +6,7 @@ package EECS2311_Project;
  * @author Team 4 EECS2311 Winter 2021
  */
 public class DrumNote {
-        //Public for testing, change to private afterwards.
-        //Each note has a measure, note number representing the 'tab' it is at (each '-' char is a note number)
-        //string number represents the string on the guitar 0 being e, 1 being A and so on.
-        //noteValue is the representation of the note in tab form, usually a combination of numbers, letters, and symbols.
+	
         public int measure;
         public int noteNumber;
         public String part;
